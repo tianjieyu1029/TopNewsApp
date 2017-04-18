41925311b2c63c84c2c35045b19e0bb083c0bf15
tianjieyu1029 <304308913@qq.com>
2017-04-18 20:36:31 +0800

    4.18

--- app/src/main/java/com/bwie/test/topnewsapp/MainActivity.java
+++ app/src/main/java/com/bwie/test/topnewsapp/MainActivity.java
@@ -5,9 +5,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,6 +24,8 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.blankj.utilcode.util.NetworkUtils;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.bwie.test.topnewsapp.activity.LoginActivity;
 import com.bwie.test.topnewsapp.adapters.MyIndicatorAdapter;
 import com.bwie.test.topnewsapp.adapters.MyViewPagerAdapter;
@@ -33,12 +39,19 @@ import com.bwie.test.topnewsapp.utils.MyXUtils;
 import com.bwie.test.topnewsapp.utils.Night_styleutils;
 import com.bwie.test.topnewsapp.utils.URLUtils;
 import com.bwie.test.topnewsapp.utils.UiUtils;
-import com.bwie.test.topnewsapp.utils.XCRoundImageView;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.auth.QQToken;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
 import net.lucode.hackware.magicindicator.MagicIndicator;
 import net.lucode.hackware.magicindicator.ViewPagerHelper;
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -61,10 +74,14 @@ public class MainActivity extends AppCompatActivity {
     private ImageView weibo_image;
     private CheckBox more_login_check;
     private RadioButton button;
-    private XCRoundImageView after_login_image;
+    private ImageView after_login_image;
     private TextView after_login_text;
     private LinearLayout after_login_layout;
     private LinearLayout before_login_layout;
+    private Tencent mTencent;
+    private UserInfo userInfo;
+    private SharedPreferences.Editor editor;
+    private int LOGINSTATE=10001;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -81,9 +98,9 @@ public class MainActivity extends AppCompatActivity {
         //判断是否有网络
         boolean connected = NetworkUtils.isConnected();
         //判断是否第一次进入
-        SharedPreferences preferences = getSharedPreferences("http", MODE_PRIVATE);
-        edit = preferences.edit();
-        flag = preferences.getBoolean("first", false);
+        SharedPreferences preferencesHttp = getSharedPreferences("http", MODE_PRIVATE);
+        edit = preferencesHttp.edit();
+        flag = preferencesHttp.getBoolean("first", false);
         if (flag){
             //读取数据库
             readDatabase();
@@ -105,6 +122,12 @@ public class MainActivity extends AppCompatActivity {
         finish();
         overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
         startActivity(intent);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+
     }
 
     //读取数据库
@@ -304,17 +327,128 @@ public class MainActivity extends AppCompatActivity {
                 reload();
             }
         });
+        qq_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
     }
+    //第三方QQ登录
+    public void login() {
+        mTencent = Tencent.createInstance("1106107726", this.getApplicationContext());
+        if (!mTencent.isSessionValid())
+        {
+
+            mTencent.login(this, "", iUiListener);
+        }
+    }
+    private IUiListener iUiListener = new IUiListener() {
+        @Override
+        public void onComplete(Object o) {
+            editor.putBoolean("flag",true);
+            editor.commit();
+            JSONObject obj = (JSONObject) o;
+            try {
+                String openID = obj.getString("openid");
+                String accessToken = obj.getString("access_token");
+                String expires = obj.getString("expires_in");
+                mTencent.setOpenId(openID);
+                mTencent.setAccessToken(accessToken, expires);
+                QQToken qqToken = mTencent.getQQToken();
+                userInfo = new UserInfo(getApplicationContext(), qqToken);
+                userInfo.getUserInfo(new IUiListener() {
+                    @Override
+                    public void onComplete(Object o) {
+
+                        JSONObject res = (JSONObject) o;
+                        //获取昵称
+                        String nickName = res.optString("nickname");
+                        //获取图片
+                        String figureurl_qq_1 = res.optString("figureurl_qq_1");
+                        editor.putString("QQName",nickName);
+                        editor.putString("QQPic",figureurl_qq_1);
+                        editor.commit();
+                        setLoginInfo(nickName, figureurl_qq_1);
+
+                    }
+
+                    @Override
+                    public void onError(UiError uiError) {
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onError(UiError uiError) {
+
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+    };
+
+    private void setLoginInfo(String nickName, String figureurl_qq_1) {
+        Glide.with(MainActivity.this).load(figureurl_qq_1).asBitmap().centerCrop().
+                into(new BitmapImageViewTarget(mine_image) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable circularBitmapDrawable =
+                                RoundedBitmapDrawableFactory.create(getResources(), resource);
+                        circularBitmapDrawable.setCircular(true);
+                        mine_image.setImageDrawable(circularBitmapDrawable);
+                    }
+                });
+
+        Glide.with(MainActivity.this).load(figureurl_qq_1).asBitmap().centerCrop().
+                into(new BitmapImageViewTarget(after_login_image) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable circularBitmapDrawable =
+                                RoundedBitmapDrawableFactory.create(getResources(), resource);
+                        circularBitmapDrawable.setCircular(true);
+                        after_login_image.setImageDrawable(circularBitmapDrawable);
+                    }
+                });
+        after_login_text.setText(nickName);
+    }
+
+    //回调
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Tencent.onActivityResultData(requestCode,resultCode,data,iUiListener);
+    }
+
     //判断显示隐藏
     @Override
     public void onResume() {
         super.onResume();
-        SharedPreferences preferences = getSharedPreferences("config", MODE_PRIVATE);
+        SharedPreferences preferencesLogin = getSharedPreferences("config", MODE_PRIVATE);
+        editor = preferencesLogin.edit();
         //flag是判断登录状态
-        boolean flag = preferences.getBoolean("flag", false);
+        boolean flag = preferencesLogin.getBoolean("flag", false);
         if (flag) {
             after_login_layout.setVisibility(View.VISIBLE);
             before_login_layout.setVisibility(View.GONE);
+           if (LOGINSTATE==Tencent.REQUEST_LOGIN){
+               String qqName = preferencesLogin.getString("QQName","");
+               String qqPic = preferencesLogin.getString("QQPic", "");
+               setLoginInfo(qqName,qqPic);
+           }
+
+
         }else{
             after_login_layout.setVisibility(View.GONE);
             before_login_layout.setVisibility(View.VISIBLE);
@@ -349,7 +483,7 @@ public class MainActivity extends AppCompatActivity {
         weibo_image = (ImageView) view.findViewById(R.id.weibo_image);
         more_login_check = (CheckBox) view.findViewById(R.id.more_login_check);
         button = (RadioButton) view.findViewById(R.id.night_button);
-        after_login_image = (XCRoundImageView) view.findViewById(R.id.after_login_image);
+        after_login_image = (ImageView) view.findViewById(R.id.after_login_image);
         after_login_text = (TextView) view.findViewById(R.id.after_login_text);
         after_login_layout = (LinearLayout) view.findViewById(R.id.after_login_layout);
         before_login_layout = (LinearLayout) view.findViewById(R.id.before_login_layout);
